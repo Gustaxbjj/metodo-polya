abf2936be139de5d483a8aa54dda48536366e361
IFGabriel07 <gfss1@aluno.ifal.edu.br>
2025-08-13 22:29:42 -0300

    Update Problemas.js

--- src/pages/Problemas.js
+++ src/pages/Problemas.js
@@ -5,7 +5,7 @@ function Problemas() {
   return (
     <div>
       <h1>Página de Problemas</h1>
-      <p>Aqui você verá os problemas...</p>
+      <p>Apresentaremos questões matemáticas para serem resolvidas no caderno. Caso você encontre dificuldades na resolução, forneceremos uma explicação detalhada, demonstrando o processo, utilizando o Método de Polya.</p>
     </div>
   );
 }
